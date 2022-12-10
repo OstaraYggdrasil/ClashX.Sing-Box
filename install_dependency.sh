546ea6c9509009c08b2fe4fded5d959d4a204d89
mrFq1 <1xxbx0il0@mozmail.com>
2022-12-10 22:27:29 +0800

    feat: sing-box core

--- install_dependency.sh
+++ install_dependency.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 set -e
 
+echo "Unzip core files"
+cd sing-box.core
+ls
+tar -xvf sing-box*arm64*
+tar -xvf sing-box*amd64*
+
+echo "Create Universal core"
+lipo -create -output com.SagerNet.Sing-Box.ProxyConfigHelper.box sing-box*arm64*/sing-box sing-box*amd64*/sing-box
+chmod +x com.SagerNet.Sing-Box.ProxyConfigHelper.box
+
+echo "Update meta core md5 to code"
+sed -i '' "s/WOSHIZIDONGSHENGCHENGDEA/$(md5 -q com.SagerNet.Sing-Box.ProxyConfigHelper.box)/g" ../ClashX/AppDelegate.swift
+sed -n '20p' ../ClashX/AppDelegate.swift
+
+echo "Gzip Universal core"
+gzip com.SagerNet.Sing-Box.ProxyConfigHelper.box
+cp com.SagerNet.Sing-Box.ProxyConfigHelper.box.gz ../ClashX/Resources/
+cd ..
+
+
+
 echo "Pod install"
 pod install
 echo "delete old files"
